d8cfdc18d47c0e1062928a29bc4173d5d3d0b5cb
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-26 18:33:56 -0700

    Update Create-a-frame.js

--- Create-a-frame.js
+++ Create-a-frame.js
@@ -48,3 +48,11 @@ const frame = (text, char) => {
 };
 
 // or
+
+const asciiFrame = (text, char) => {
+  const width = text.reduce((maxWidth, v) => v.length > maxWidth ? v.length : maxWidth, 0)
+  const line = content => `${char} ${content.padEnd(width, ' ')} ${char}`
+  const edge = char.repeat(width + 4)
+  
+  return [edge, ...text.map(line), edge].join('\n');
+};
